2d3e66d19e99d260ba1e93da26705aad10954ac1
Jesse Jurman <j.r.jurman@gmail.com>
2013-02-10 17:21:26 -0500

    Description of test

--- test/my_test.rb
+++ test/my_test.rb
@@ -1,9 +1,13 @@
+# This is my own little test for checking functionality,
+# It's not very formal, but it shows some of the methods
+# and their usage, so feel free to branch off this example
+
 require '../lib/console_splash.rb'
 
 splsh = Console_Splash.new()
 splsh.write_vertical_pattern("^_")
 splsh.write_horizontal_pattern("<_")
-splsh.write_center(-3, "Hello Nurse")
+splsh.write_center(-3, "Splash Screens are most Excellent!")
 splsh.write_header("console_splash", "Jesse Jurman", "0.0.1")
 splsh.splash
 sleep(3)
